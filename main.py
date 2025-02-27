c67a4978356882aa1b1d8405885528b4d76cd80c
Aiden <uefadude@gmail.com>
2025-02-27 08:07:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import pandas as pd
+
+
+# API Key: AgBYFvqpC2sc6UqooUS3OPCRuw9qp6dB
+# Get data for a specific stock over a given range
+r = requests.get('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=AgBYFvqpC2sc6UqooUS3OPCRuw9qp6dB')
+response_data = r.json()
+
+print(f'JSON is: {response_data}')
+print(response_data.get('results'))
+# c (close), h (highest), l (lowest), n (number of transactions), o (open), otc (OTC ticker or not), t (timestamp), v (trading volume), vw (volume weighted average price)
+
+
+r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&apiKey=AgBYFvqpC2sc6UqooUS3OPCRuw9qp6dB')
+response_data = r.json()
+print(f'JSON is: {response_data}')
+print(response_data.get('results'))
+
+df = pd.DataFrame(response_data['results'])
