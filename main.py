20b92abdc7a0f7d64a8f8f6ecc0137e037e9612b
Aiden Egglin <uefadude@gmail.com>
2025-03-05 16:14:50 -0500

    Start graphing and move transforms to function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import logging
 import json
 import random
-import requests
 
+import requests
 import pandas as pd
 
 from secrets import ALPHA_VANTAGE_API_KEY
@@ -15,7 +16,7 @@ def query_time_series_intraday_api(symbol, interval="60min", adjusted="true"):
     r = requests.get(url)
     data = r.json()
 
-    print(json.dumps(data, indent=4))
+    log.info(json.dumps(data, indent=4))
 
     return data
 
@@ -24,9 +25,7 @@ def clean_data(data, symbol, interval):
 
     time_series = data[symbol][f"Time Series ({interval})"]
     for date, values in time_series.items():
-
         cleaned_dict = {}
-
         for stat_name, value in values.items():
             stat_name = (
                 stat_name.replace("1. ", "")
@@ -39,8 +38,26 @@ def clean_data(data, symbol, interval):
 
         time_series[date] = cleaned_dict
 
-    return pd.DataFrame(time_series)
+    stocks_df = pd.DataFrame(time_series)
+    # Transpose the dataframe so the dates are rows
+    stocks_df = stocks_df.T
 
+    # Update types
+    stocks_df.index = pd.to_datetime(stocks_df.index)
+    cols = ['open', 'high', 'low', 'close', 'volume']
+    stocks_df[cols] = stocks_df[cols].astype(float)
+
+    return stocks_df
+
+
+def show_graphs(df):
+
+    plt.style.use('fivethirtyeight')
+    y1 = 'open'
+
+    fig, ax = plt.subplots()
+    ax.plot(df.index, df['open'])
+    plt.show()
 
 def main():
 
@@ -147,18 +164,15 @@ def main():
         "BIIB",
     ]
 
-    chosen_symbol = random.choices(
+    chosen_symbols = random.choices(
         nasdaq_100, k=1
     )  # temporarily use one symbol to test
     stock_data = {
-        symbol: query_time_series_intraday_api(symbol) for symbol in chosen_symbol
+        symbol: query_time_series_intraday_api(symbol) for symbol in chosen_symbols
     }
 
-    stocks_df = clean_data(stock_data, chosen_symbol, "60min")
-
-    # Transpose the dataframe so the dates are rows
-    stocks_df = stocks_df.T
-    print(stock_data.head())
+    stocks_df = clean_data(stock_data, chosen_symbol[0], "60min")
+    log.info(stocks_df.head())
 
 
 if __name__ == "__main__":
